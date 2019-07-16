32b1636f28296adcce960d52c70e163a179af830
Wataru Katsuki <a17dc050@dhw.ac.jp>
2019-07-16 20:21:34 +0900

    add index

--- work/index.php
+++ work/index.php
@@ -1,3 +1,27 @@
-<?
-  echo "hello world";
-?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <header>
+  <div>
+    <h1>CMS</h1>
+    <span>Logout</span>
+  </div>
+  </header>
+  <form action="">
+    <div>
+      <p>Email</p>
+      <input type="text">
+    </div>
+    <div>
+      <p>Password</p>
+      <input type="text">
+    </div>
+  </form>
+</body>
+</html>
