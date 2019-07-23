6c562290cb77427cc86e2835f7c21b668966babd
Wataru Katsuki <a17dc050@dhw.ac.jp>
2019-07-23 19:00:40 +0900

    fix database

--- work/sql/cms.sql
+++ work/sql/cms.sql
@@ -4,37 +4,38 @@ GRANT ALL ON *.* TO myuser@'%' IDENTIFIED BY 'testuser' WITH GRANT OPTION;
 create database cms default charset utf8;
 use cms;
 
-create table category(
-    id int auto_increment primary key,
-    name varchar(255) not null
+create table users(
+    id int auto_increment not null primary key,
+    username varchar(20) not null,
+    password varchar(100) not null
+);
+
+create table permissions(
+    id int auto_increment not null primary key,
+    name varchar(20) not null
+);
+
+create table user_permissions(
+    id int auto_increment not null primary key,
+    user_id int not null,
+    permission_id int not null,
+    foreign key (user_id)
+        references users(id),
+    foreign key (permission_id)
+        references permissions(id)
+);
+
+create table categories(
+    id int auto_increment not null primary key,
+    name varchar(30) not null
 );
 
 create table contents(
-    id int auto_increment primary key,
+    id int auto_increment not null primary key,
     category_id int not null,
+    title varchar(50) not null,
+    body varchar(255) not null,
+    date timestamp default current_timestamp,
     foreign key (category_id)
-      references category(id),
-    title varchar(255),
-    created varchar(255)
-);
-
-create table user(
-    id int auto_increment primary key,
-    name varchar(255) not null,
-    user_type varchar(255) not null
-);
-
-create table permission(
-    id int auto_increment primary key,
-    name varchar(255) not null
-);
-
-create table user_permission(
-    id int auto_increment primary key,
-    user_id int not null,
-    foreign key (user_id)
-      references user(id),
-    permission int not null,
-    foreign key (permission)
-      references permission(id)
+        references categories(id)
 );
