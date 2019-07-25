ff4d954766e50bf241128026cc9a25edd94dea55
Wataru Katsuki <a17dc050@dhw.ac.jp>
2019-07-25 22:16:32 +0900

    add user_permissions data

--- work/user_register.php
+++ work/user_register.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
 </head>
-<body>　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　
+<body>　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　
   <header>
   <div>
     <h1>CMS</h1>
@@ -24,9 +24,9 @@
     </div>
     <div>
       <p>権限</p>
-      <input type="checkbox" name="permission" value="administrator">管理者
-      <input type="checkbox" name="permission" value="editor">編集者
-      <input type="checkbox" name="permission" value="viewer">閲覧者
+      <input type="radio" name="permission" value="1">管理者
+      <input type="radio" name="permission" value="2">編集者
+      <input type="radio" name="permission" value="3">閲覧者
     </div>
     <button>登録</button>
   </form>
@@ -34,31 +34,20 @@
   try {
     $dbh = new PDO('mysql:host=db;dbname=cms','myuser', 'testuser');
     $stmt = $dbh->prepare(
-    'INSERT INTO user (username, password) VALUES (?,?)'
+    'INSERT INTO users (username, password) VALUES (?,?)'
     );
     $id = $_POST['id'];
     $pass = $_POST['pass'];
     $stmt->execute(array($id, $pass));
 
     $stmt = $dbh->query('select max(id) from users ');
-    var_dump($stmt);
     if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-      $max_id = $row['max(ID)'];
       $permission = $_POST['permission'];
       $_stmt = $dbh->prepare(
-        'INSERT INTO user_permission (user_id, permission_id) VALUES (?,?)'
+        'INSERT INTO user_permissions (user_id, permission_id) VALUES (?,?)'
       );
-      foreach ($permission as $perm) {
-        $_stmt->execute($max_id, $perm);
-      };
+      $_stmt->execute(array($row["max(id)"], $permission));
     }
-    // $stmt = $dbh->prepare(
-    //   'INSERT INTO user_permissions (user_id, permission_id) VALUES (?,?)'
-    // );
-
-    // $permission = $_POST['permission'];
-
-    // $stmt->execute($permission);
 
     // header('Location: index.php');
   } catch (PDOException $e) {
