9438d963f5dfb33824ab27fe57309d46819e0292
Wataru Katsuki <a17dc050@dhw.ac.jp>
2019-07-29 13:48:31 +0900

    add user list

--- work/user_list.php
+++ work/user_list.php
@@ -1,12 +1,6 @@
 <?php
   $dbh = new PDO('mysql:host=db;dbname=cms','myuser', 'testuser');
   session_start();
-  $perm = $_SESSION['perm'];
-  $permname = $_SESSION['permname'];
-  $user = $_SESSION['user'];
-  if(!isset($user)) {
-    header('Location: login.php');
-  }
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -18,14 +12,30 @@
 </head>
 <body>
   <h1>ユーザー一覧</h1>
-  <?php
-  var_dump($_SESSION['row']);
-  var_dump($user);
-  var_dump($perm);
-  var_dump($permname);
-  if (in_array(1, $perm)) { ?>
-  <?php
+<?php
+  $stmt = $dbh->prepare('SELECT users.id, users.username FROM users');
+  $stmt->execute();
+  while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+?>
+  <div>
+    <p><?= $row['id']?></p>
+    <p><?= $row['username']?></p>
+    <div>
+  <?
+    $_stmt = $dbh->prepare(
+      'SELECT * FROM permissions JOIN user_permissions ON permissions.id = user_permissions.permission_id WHERE user_id = ?'
+    );
+    $_stmt->execute([$row['id']]);
+    while($_row = $_stmt->fetch(PDO::FETCH_ASSOC)) {
+      ?>
+    <p><?= $_row['name']?></p>
+  <?
   }
   ?>
+    </div>
+  </div>
+<?
+  }
+?>
 </body>
 </html>
