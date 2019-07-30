ac11111eafee7a7901357f58a37af36fa6bb5ce3
Wataru Katsuki <a17dc050@dhw.ac.jp>
2019-07-30 13:49:25 +0900

    add category list

---
+++ work/category_list.php
@@ -0,0 +1,30 @@
+<?php
+  $dbh = new PDO('mysql:host=db;dbname=cms','myuser', 'testuser');
+  session_start();
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>CMS　カテゴリー一覧</title>
+  <link rel="stylesheet" href="reset.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>カテゴリー一覧</h1>
+<?php
+  $stmt = $dbh->prepare('SELECT * FROM categories');
+  $stmt->execute();
+  while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+?>
+  <div class="category_list">
+    <p class="categories_id"><?= $row['id']?></p>
+    <p class="categories_name"><?= $row['name']?></p>
+  </div>
+<?
+  }
+?>
+</body>
+</html>
